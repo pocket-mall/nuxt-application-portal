9c07d496f0168b88bc3be851a814219c76695920
zychen96 <zhongyi96chen@gmail.com>
2023-02-10 21:31:52 +0100

    refactor: eslint fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    nitro: {
-        preset: "deno"
-    }
-})
+  nitro: {
+    preset: "deno",
+  },
+});
